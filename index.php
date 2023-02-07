4e1f9b332d4980f72242efe55f751677c058b0b8
boon <39808013+MaxPearce4444@users.noreply.github.com>
2023-02-07 20:28:52 +0000

    Create index.php

---
+++ index.php
@@ -0,0 +1,86 @@
+<body class="lemonmilk">
+<meta http-equiv="content-type" content="text/html;charset=UTF-8" />
+<head>
+<meta charset="utf-8">
+<meta name="viewport" content="width=device-width, initial-scale=1.0, shrink-to-fit=no">
+<title>boon</title>
+<link rel="icon" type="image/x-icon" href="https://boonz.co.uk/images/cookie.jpg">
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<meta name="theme-color" content="#992DFF">
+<meta property="og:description" content="boon is best.">
+<meta property="og:title" content="boons.pics">
+<link rel="stylesheet" href="static/css/bootstrap.min.css">
+<link rel="stylesheet" href="static/css/styles.css?v=<?php echo time(); ?>">
+<link rel="stylesheet" href="static/css/Navigation-Clean.css">
+<body class="bmain">
+<script>
+      var _app = function () {
+          this.id = 0;
+          this.videoElement = null;
+          this.musicVolume = 0.00;
+          this.musicFadeIn = 4000;
+          this.Texts = [
+            "Have a good time here, ❤️"
+          ];
+      };
+      
+      var app = new _app();
+    </script>
+<div class="background">
+<div id="pattern"></div>
+<video id="background" loop="">
+<source src="static/videos/starfall.mp4" type="video/mp4">
+<script>
+          var video = document.getElementById("background");
+          app.videoElement = video;
+          app.videoElement.volume = 0;
+        </script>
+</video>
+</div>
+<div>
+<nav class="navbar navbar-light navbar-expand-lg navigation-clean">
+<div class="container">
+<div class="collapse navbar-collapse" id="navcol-1">
+<ul class="navbar-nav mx-auto">
+<li class="nav-item"><a class="nav-link active" href="index">> Home </a></li>
+<li class="nav-item"><a class="nav-link" href="/projects/index.php">> Projects</a></li>
+<li class="nav-item"><a class="nav-link" href="https://discord.com/users/327911856628826112">> Discord </a></li>
+<li class="nav-item"><a class="nav-link" href="https://wtf.boons.pics/">> Bios </a></li>
+</ul>
+</div>
+</nav>
+<section class="main">
+<div class="text-start containers">
+<div class="Brand">
+
+</div>
+<a class="yt" href="https://www.youtube.com/@lovelyboon1981" target="_blank"> <img src="static\images\youtube2.png" width="60" height="60"> </a>
+<a class="discord" href="https://discord.com/users/327911856628826112" target="_blank"> <img src="static\images\discord2.png" width="60" height="60"> </a>
+<a class="spotify" href="https://open.spotify.com/user/bmprq9ex134jltu5388cjyl14?si=9e5742d101894edd" target="_blank"> <img src="static\images\spotify2.png" width="60" height="60"> </a>
+<a class="twitch" href="https://www.twitch.tv/lovelyboon" target="_blank"> <img src="static\images\twitch.png" width="60" height="60"> </a>
+<a class="steam" href="https://steamcommunity.com/id/fullmetalboon" target="_blank"> <img src="static\images\steam.png" width="60" height="60"> </a>
+<a class="vrc" href="https://vrchat.com/home/user/usr_5746bccc-7484-467e-9e5c-02580548be69" target="_blank"> <img src="static\images\vrc.png" width="60" height="60"> </a>
+
+  <h1 class="Paragraph Hello" id="testamonguspoop"></h1>
+<div class="egirl">
+     <audio controls disablePictureInPicture controlsList="nodownload nodirector" noPlaybackRate>
+  <source src="/static/videos/egirlboon.mp3" type="audio/mpeg">
+</audio>
+</div>
+<p class="a"><span id="Bruh" class="Paragraph"></span></p>
+</div>
+<iframe height="250px" width="410px" scrolling="no" src="https://lanyard.cnrad.dev/api/327911856628826112?borderRadius=5px&idleMessag=And%20i've%20tried%20to%20just%20be%20me,%20like%20a%20thousand%20times%20but%20you're%20on%20my%20mind%20lol&bg=a&animated=true;%22" frameborder="0"></iframe>
+</div>
+</section>
+</div>
+
+
+</div>
+
+<script src="static/js/bootstrap.min.js"></script>
+<script src="static/js/jquery.js"></script>
+<script src="static/js/jquery_cookie.js"></script>
+<script src="static/js/typed.js"></script>
+<script src="static/js/ok3.js"></script>
+</body>
+</html>
